d113c7cfe87a628131eeca489f2280ecbad471aa
Portakof <elektrakof@gmail.com>
2019-09-09 23:57:12 -0500

    #7 Curso de C# .NET Vistas en MVC .Net, Razor

--- ChatWeb/Controllers/HomeController.cs
+++ ChatWeb/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using UtilitiesChat;
 using ChatWeb.Business;
 
@@ -11,31 +7,31 @@ namespace ChatWeb.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {        
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            ChatWeb.Models.ViewModels.RegisterViewModel model = new Models.ViewModels.RegisterViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Register(ChatWeb.Models.ViewModels.RegisterViewModel model)
         {
             Models.Request.User oUser = new Models.Request.User();
-            oUser.Name = "Gatito";
-            oUser.City = "San Pascual";
-            oUser.Email = "Ratoncito@mail.com";
-            oUser.Password = "123456";
+            oUser.Name = model.Name;
+            oUser.City = model.City;
+            oUser.Email = model.Email;
+            oUser.Password = model.Password;
 
             RequestUtil oRequesUtil = new RequestUtil();
-            UtilitiesChat.Models.WS.Reply oReply = oRequesUtil.Execute<Models.Request.User>(Constants.Url.REGISTER,"post",oUser);
+            UtilitiesChat.Models.WS.Reply oReply = oRequesUtil.Execute<Models.Request.User>(Constants.Url.REGISTER, "post", oUser);
 
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-            
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ ChatWeb/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatWeb.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string Name {get; set;}
+        public string City {get; set;}
+        public string Password {get; set;}
+        public string Email {get; set;}
+}
+}
